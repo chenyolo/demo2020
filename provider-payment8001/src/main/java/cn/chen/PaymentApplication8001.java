ee857c22cad7e3784f61a318fd915f3e67d5287b
JayinChan <719045094@qq.com>
2020-05-28 09:45:41 +0800

    服务发现

--- provider-payment8001/src/main/java/cn/chen/PaymentApplication8001.java
+++ provider-payment8001/src/main/java/cn/chen/PaymentApplication8001.java
@@ -2,6 +2,7 @@ package cn.chen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -15,6 +16,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient // 客户端
+@EnableDiscoveryClient //服务发现
 public class PaymentApplication8001 {
 
 	public static void main(String[] args) {
--- provider-payment8001/src/main/java/cn/chen/web/PaymentController.java
+++ provider-payment8001/src/main/java/cn/chen/web/PaymentController.java
@@ -7,7 +7,11 @@ import cn.chen.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @version demo2020-1.0
@@ -25,8 +29,14 @@ public class PaymentController {
 	@Autowired
 	private PaymentService paymentService;
 
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
 	@Value("${server.port}")
 	private String serverPort;
+
+	@Value("${spring.application.name}")
+	private String appName;
 
 	@PostMapping("/save")
 	public CommonResult<Payment> save(Payment payment){
@@ -44,6 +54,19 @@ public class PaymentController {
 			return new CommonResult<Payment>(ResultCode.CODE_SUCCESS,"查询成功!serverPort:"+serverPort, payment);
 		else
 			return new CommonResult<Payment>(ResultCode.CODE_FAILED,"查询无结果");
+	}
+
+	@GetMapping(value = "/discovery")
+	public Object discovery(){
+		List<String> services = discoveryClient.getServices();
+		for (String element : services) {
+			log.info("***** element:"+element);
+		}
+		List<ServiceInstance> instances = discoveryClient.getInstances(appName);
+		for (ServiceInstance instance : instances) {
+			log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+		}
+		return this.discoveryClient;
 	}
 
 }
